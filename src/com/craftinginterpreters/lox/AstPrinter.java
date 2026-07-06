8177964d1edf4d7f5044d661c0fa8aadb6faa9a0
Sanjana Mandadi <sanjanamandadi@gmail.com>
2026-07-06 19:24:52 -0400

    Adding ch. 9 (control flow) files

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -8,6 +8,11 @@ class AstPrinter implements Expr.Visitor<String> {
   }
 
   @Override
+  public String visitLogicalExpr(Expr.Logical expr) {
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  @Override
   public String visitBinaryExpr(Expr.Binary expr) {
     return parenthesize(expr.operator.lexeme,
                         expr.left, expr.right);
